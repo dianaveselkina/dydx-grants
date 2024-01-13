b46bb2c8ac5e118a7b46f1162e582b517e984e40
dianaveselkina <vess_di@mail.ru>
2024-01-13 23:42:11 +0300

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
+import { Hero } from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
       <Footer />
     </div>
   );
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.css';
+import { Button } from '../Button/Button';
+
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="container">
+        <h1 className="hero__title">
+          Powering the future of dYdX through community grants
+        </h1>
+        <Button />
+        <Button />
+        <div className="hero__info">
+          <div>
+            <p>100+</p>
+            <p>projects funded</p>
+          </div>
+          <div>
+            <p>$3+ milion</p>
+            <p>awarded</p>
+          </div>
+          <div>
+            <p>400+</p>
+            <p>projects funded</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
