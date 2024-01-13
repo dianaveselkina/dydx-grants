fc2e7e7e00c99d75182b85ac630657a429a6a329
dianaveselkina <vess_di@mail.ru>
2024-01-13 22:32:20 +0300

    style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ReactComponent as Logo } from './img/Logo.svg';
 import { Button } from '../Button/Button';
+import './index.css';
 
 export const Header = () => {
   return (
@@ -25,9 +26,9 @@ export const Header = () => {
               <li>
                 <a href="/">FAQ</a>
               </li>
+              <Button />
             </ul>
           </nav>
-          <Button />
         </div>
       </div>
     </header>
