e22aedd4ca947d4d5440032f54d2ad4953a56fc3
dianaveselkina <vess_di@mail.ru>
2024-01-16 21:32:49 +0300

    add  Process

---
+++ src/components/Process/Process.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export const Process = () => {
+  return (
+    <div className="process">
+      <div className="container"></div>
+    </div>
+  );
+};
