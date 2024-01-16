7b0175f6e684f172d2989e34c45d8691e4ccdf96
dianaveselkina <vess_di@mail.ru>
2024-01-16 20:39:06 +0300

    add block

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Featured } from './components/Featured/Featured';
 import { Footer } from './components/Footer/Footer';
+import { Frequently } from './components/Frequently/Frequently';
 import { Header } from './components/Header/Header';
 import { Hero } from './components/Hero/Hero';
 import { News } from './components/News/News';
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Featured />
       <News />
+      <Frequently />
       <Footer />
     </div>
   );
--- src/components/Frequently/Frequently.jsx
+++ src/components/Frequently/Frequently.jsx
@@ -4,7 +4,45 @@ import './index.css';
 export const Frequently = () => {
   return (
     <div className="frequently">
-      <div className="container"></div>
+      <div className="container">
+        <h2 className="frequently__title">Frequently asked questions</h2>
+        <p className="frequently__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nulla
+          risus, consectetur ut bibendum non, gravida non libero.
+        </p>
+        <div className="frequently__block">
+          <div className="block__ask">
+            <p>How do I apply?</p>
+            <span>+</span>
+          </div>
+          <div className="block__ask">
+            <p>Can I apply as a U.S. based person/company?</p>
+            <span>+</span>
+          </div>
+          <div className="block__ask">
+            <p>How will I be compensated?</p>
+            <span>+</span>
+          </div>
+          <div className="block__ask">
+            <p>What type of project will qualify for a Grant?</p>
+            <span>+</span>
+          </div>
+          <div className="block__ask">
+            <p>
+              What is the application timeline? When can I expect to hear back?
+            </p>
+            <span>+</span>
+          </div>
+          <div className="block__ask">
+            <p>How can I increase my chances of getting funded?</p>
+            <span>+</span>
+          </div>
+          <div className="block__ask">
+            <p>How often do you approve new Rounds of funding?</p>
+            <span>+</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
