cce2a88c0ae3a9b3ce2fe05582b3c10a02295a3e
dianaveselkina <vess_di@mail.ru>
2024-01-16 23:06:27 +0300

    add icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './index.css';
 import { ReactComponent as LogoFooter } from './img/logo-footer.svg';
-
+import { ReactComponent as Arrow } from './img/icon-arrow.svg';
+import { ReactComponent as Twitter } from './img/twitter.svg';
+import { ReactComponent as Discord } from './img/discord.svg';
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -20,15 +22,24 @@ export const Footer = () => {
                 <a href="/">Brand assets</a>
               </li>
               <li>
-                <a href="/">dYdX Foundation</a>
+                <a href="/">
+                  dYdX Foundation
+                  <Arrow />
+                </a>
               </li>
               <li>
-                <a href="/">dYdX trading</a>
+                <a href="/">
+                  dYdX trading
+                  <Arrow />
+                </a>
               </li>
             </ul>
           </nav>
         </div>
-        <div className="footer__social"></div>
+        <div className="footer__social">
+          <Twitter />
+          <Discord />
+        </div>
       </div>
     </footer>
   );
