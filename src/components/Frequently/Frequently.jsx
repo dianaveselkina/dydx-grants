2b98d92aacc0cb2184c0e9abe61549cfa10449b7
dianaveselkina <vess_di@mail.ru>
2024-01-16 20:30:52 +0300

    add Frequently

---
+++ src/components/Frequently/Frequently.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export const Frequently = () => {
+  return (
+    <div className="frequently">
+      <div className="container"></div>
+    </div>
+  );
+};
