650edb742c47775724801cd6c9c32b79d9dc6df5
dianaveselkina <vess_di@mail.ru>
2024-01-16 22:41:34 +0300

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import './index.css';
+import { ReactComponent as LogoFooter } from './img/logo-footer.svg';
 
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__wrapper">
+          <LogoFooter />
+          <nav className="footer__nav">
+            <ul class="nav__list">
+              <li>
+                <a href="/">About</a>
+              </li>
+              <li>
+                <a href="/">FAQ</a>
+              </li>
+              <li>
+                <a href="/">Brand assets</a>
+              </li>
+              <li>
+                <a href="/">dYdX Foundation</a>
+              </li>
+              <li>
+                <a href="/">dYdX trading</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="footer__social"></div>
+      </div>
+    </footer>
+  );
 };
