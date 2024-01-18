a93b736623ad088be2a08c75047d8b283dbe2494
dianaveselkina <vess_di@mail.ru>
2024-01-18 20:09:53 +0300

    add DiscoversPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { DiscoversPage } from './pages/DiscoversPage';
 import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <DiscoversPage />
     </div>
   );
 }
---
+++ src/pages/DiscoversPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from '../components/Header/Header';
+
+import { Creation } from '../components/Creation/Creation';
+import { Footer } from '../components/Footer/Footer';
+
+export const DiscoversPage = () => {
+  return (
+    <div>
+      <Header />
+
+      <Creation />
+      <Footer />
+    </div>
+  );
+};
