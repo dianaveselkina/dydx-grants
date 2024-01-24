938e661c1d4731b35ab64e675ad9cd3bf90c585b
dianaveselkina <vess_di@mail.ru>
2024-01-24 18:37:42 +0300

    fix style

--- src/pages/QuestionsPage.jsx
+++ src/pages/QuestionsPage.jsx
@@ -3,7 +3,6 @@ import { Header } from '../components/Header/Header';
 import { Creation } from '../components/Creation/Creation';
 import { Footer } from '../components/Footer/Footer';
 import { Accordion, AccordionItem } from '@szhsin/react-accordion';
-
 import './index.css';
 
 export const QuestionsPage = () => {
